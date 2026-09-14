e3f689e7ff54fdb0ce5e184982aef272a014323a
gonshalesh <2gonzalezkevin@gmail.com>
2026-09-14 10:01:52 -0700

    comment two sum solution

--- Data Structures & Algorithms/two-integer-sum/submission-12.py
+++ Data Structures & Algorithms/two-integer-sum/submission-12.py
@@ -1,13 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        # create hashmap to store seen numbers and their indexes
         seen = {}
 
+        # iterate through array
         for i in range(len(nums)):
-                key = nums[i]
-                diff = target - key
+                key = nums[i] # current number
+                diff = target - key # calculate the complement needed to reach target
 
+                # if the complement exists in our hashmap
                 if diff in seen:
-                    return [seen[diff], i]
+                    return [seen[diff], i] # return both indexes
 
-                seen[key] = i
+                seen[key] = i # store current number and its index for future checks
