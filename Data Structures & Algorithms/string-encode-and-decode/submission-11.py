67717eb9708170d3b74f2213ca348f1d441dde49
gonshalesh <2gonzalezkevin@gmail.com>
2026-09-15 15:42:53 -0700

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.py
@@ -0,0 +1,34 @@
+# Best space complexity, 1st submission uses more but still in linear time
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        output = [] # create empty list for encoded output
+        for word in strs: # iterate through each string in list
+            output.append(f"{len(word)}#{word}") # create encoded string
+
+        # each word looks like this after encoding
+        # 4#word
+
+        return "".join(output) # return encoded strings as one long string
+
+    def decode(self, s: str) -> List[str]:
+        words = [] # store each decoded word
+        i = 0 # position of the start of encode() output
+
+        # word format, ex: 4#word
+        # [word_length][current_pound][word_start][word_end]
+        while i < len(s): # process until we reach the end of the encoded string
+            current_pound = s.index("#", i) # from index i, find first instance of '#'
+            word_length = int(s[i:current_pound]) # read the length number left of current_pound
+
+            word_start = current_pound + 1 # word begins immediately after "#"
+            word_end = word_start + word_length # word ends after word_length characters
+
+            # where actual logic executes
+            words.append(s[word_start:word_end]) # extract the word and store it (between word_start and word_end)
+
+            i = word_end #redeclare position to be the end of whatever word we just retrieved
+
+        return words # return all decoded words
