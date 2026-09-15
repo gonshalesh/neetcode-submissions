980576b84f00cf7a628d31c9bb5a669678cb3427
gonshalesh <2gonzalezkevin@gmail.com>
2026-09-15 13:23:06 -0700

    updated comments again

--- Data Structures & Algorithms/string-encode-and-decode/submission-9.py
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -15,16 +15,18 @@ class Solution:
 
     def decode(self, s: str) -> List[str]:
         words = [] # store each decoded word
-        i = 0 # position of the start of each encoded word (which is also its length)
+        i = 0 # current position in the encoded string
 
+        # word format, ex: 4#word
+        # [word_length][current_pound][word_start][word_end]
         while i < len(s): # process until we reach the end of the encoded string
-            current_pound = s.index("#", i) # find the "#" dividing length from word
-            word_length = int(s[i:current_pound]) # read the length number left of "#"
+            current_pound = s.index("#", i) # from index i, find first instance of '#'
+            word_length = int(s[i:current_pound]) # read the length number left of current_pound
 
             word_start = current_pound + 1 # word begins immediately after "#"
             word_end = word_start + word_length # word ends after word_length characters
 
-            words.append(s[word_start:word_end]) # extract the word and store it
-            i = word_end # move to the length number of the next section
+            words.append(s[word_start:word_end]) # extract the word and store it (between word_start and word_end)
+            i = word_end # i now equals position to be the end of whatever word we just retrieved
 
         return words # return all decoded words
